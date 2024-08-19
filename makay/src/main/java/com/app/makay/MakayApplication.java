060df228a0f6b2321eca12b646d70ffc1bb112b9
Eriq606 <erohweltall@gmail.com>
2024-08-19 15:18:04 +0300

    initial commit spring

---
+++ makay/src/main/java/com/app/makay/MakayApplication.java
@@ -0,0 +1,13 @@
+package com.app.makay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakayApplication.class, args);
+	}
+
+}
---
+++ makay/src/test/java/com/app/makay/MakayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.makay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
