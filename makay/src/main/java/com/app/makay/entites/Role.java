3e211eea98ccd05abb15ad917ce9b8515755e117
Eriq606 <erohweltall@gmail.com>
2024-08-20 09:59:01 +0300

    création des entités

---
+++ makay/src/main/java/com/app/makay/entites/Role.java
@@ -0,0 +1,43 @@
+package com.app.makay.entites;
+
+import veda.godao.annotations.Column;
+import veda.godao.annotations.PrimaryKey;
+import veda.godao.annotations.Table;
+
+@Table("roles")
+public class Role {
+    @PrimaryKey
+    @Column("id")
+    private Integer id;
+    @Column("nom")
+    private String nom;
+    @Column("autorisation")
+    private Integer autorisation;
+    @Column("etat")
+    private Integer etat;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public Integer getAutorisation() {
+        return autorisation;
+    }
+    public void setAutorisation(Integer autorisation) {
+        this.autorisation = autorisation;
+    }
+    public Integer getEtat() {
+        return etat;
+    }
+    public void setEtat(Integer etat) {
+        this.etat = etat;
+    }
+    
+}
--- makay/src/main/java/com/app/makay/entites/Utilisateur.java
+++ makay/src/main/java/com/app/makay/entites/Utilisateur.java
@@ -1,5 +1,68 @@
 package com.app.makay.entites;
 
+import veda.godao.annotations.Column;
+import veda.godao.annotations.ForeignKey;
+import veda.godao.annotations.PrimaryKey;
+
 public class Utilisateur {
+    @PrimaryKey
+    @Column("id")
+    private Integer id;
+    @Column("nom")
+    private String nom;
+    @Column("email")
+    private String email;
+    @Column("contact")
+    private String contact;
+    @Column("motdepasse")
+    private String motdepasse;
+    @ForeignKey(recursive = true)
+    @Column("idrole")
+    private Role role;
+    @Column("etat")
+    private Integer etat;
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getContact() {
+        return contact;
+    }
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+    public String getMotdepasse() {
+        return motdepasse;
+    }
+    public void setMotdepasse(String motdepasse) {
+        this.motdepasse = motdepasse;
+    }
+    public Integer getEtat() {
+        return etat;
+    }
+    public void setEtat(Integer etat) {
+        this.etat = etat;
+    }
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
     
 }
