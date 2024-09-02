e29969c122c29b9bb301a8bd258ba235f928ea07
Eriq606 <erohweltall@gmail.com>
2024-09-02 10:21:49 +0300

    test rest in mvc

--- makay/src/main/java/com/app/makay/controllers/metier/superviseur/SuperviseurController.java
+++ makay/src/main/java/com/app/makay/controllers/metier/superviseur/SuperviseurController.java
@@ -3,14 +3,17 @@ package com.app.makay.controllers.metier.superviseur;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.app.makay.entites.Categorie;
 import com.app.makay.entites.HistoriqueRoleUtilisateur;
 import com.app.makay.entites.Place;
 import com.app.makay.entites.Rangee;
@@ -97,12 +100,11 @@ public class SuperviseurController {
     }
 
     @GetMapping("/try-me")
-    public void categorie(){
-        try{
-            System.out.println("hello");
-        }catch(Exception e){
-            System.out.println("Default error");
-        }
+    @ResponseBody
+    public ResponseEntity<Categorie> categorie(){
+        Categorie cat=new Categorie();
+        cat.setNom("Hall");
+        return ResponseEntity.ok(cat);
     }
 
     @MessageMapping("/")
