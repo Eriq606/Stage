c99a84ed66d3dae142c76560ee6cca9b7eba5479
Eriq606 <erohweltall@gmail.com>
2024-09-04 15:03:18 +0300

    envoi de la motification par JS

--- makay/src/main/java/com/app/makay/controllers/metier/superviseur/SuperviseurController.java
+++ makay/src/main/java/com/app/makay/controllers/metier/superviseur/SuperviseurController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.annotation.SessionScope;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.app.makay.entites.HistoriqueRoleUtilisateur;
@@ -36,7 +35,6 @@ import jakarta.servlet.http.HttpSession;
 import veda.godao.utils.DAOConnexion;
 
 @Controller
-@SessionScope
 public class SuperviseurController {
     private MyFilter filter;
     private Place[] places;
